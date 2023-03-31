5c044af04e85fec364fa53bba68811105bcf5bf3
Ivan Souza <ivan.lmedeiros76@gmail.com>
2023-03-31 10:39:03 -0400

    projeto foi movido

--- testandojava/testandojava/src/GeradoraDeFigurinha.java
+++ testandojava/testandojava/src/GeradoraDeFigurinha.java
@@ -1,4 +1,39 @@
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.File;
+
+
+import javax.imageio.ImageIO;
+
 public class GeradoraDeFigurinha {
     
-    
+    public void cria() {
+        try{
+        BufferedImage imagemOriginal = ImageIO.read(new File("entrada/imagem.jpg"));
+
+        int largura = imagemOriginal.getWidth();
+        int altura = imagemOriginal.getHeight();
+        int novaAltura = altura + 200;
+        var novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
+
+        Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
+
+        graphics.drawImage(imagemOriginal, 0, 0, null);
+
+        
+
+        
+        ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
+        }catch(Exception exception){
+            System.out.println(exception);
+        }
+        
+
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+        var geradora = new GeradoraDeFigurinha();
+        geradora.cria();
+    }
 }
